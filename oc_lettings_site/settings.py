dfe57475d9454cf7464bd9b064c056a29eaed76d
Rochdi GUEZGUEZ <rochdi.guezguez@gmail.com>
2023-06-29 18:11:23 +0100

    Update oc_lettings_site

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -33,8 +33,8 @@ env.read_env()
 SECRET_KEY = env.str("SECRET_KEY", default="fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = env.bool("DEBUG", default=False)
+# DEBUG = True
+DEBUG = env.bool("DEBUG", default=False)
 
 ALLOWED_HOSTS = [".herokuapp.com", ".localhost", "127.0.0.1"]
 
